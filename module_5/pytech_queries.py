0d1d824dc3aa15cd2dd7c5444de203544fd4d70d
Benjamin Clough <bclough@my365.bellevue.edu>
2023-01-15 21:17:38 -0600

    Update to the module_5 directory. Both programs have permissions errors when attempting to connect to Atlas DB.

---
+++ module_5/pytech_queries.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.kirb2v7.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(url)
+
+db = client.pytech
+
+collection = db.students
+
+students = db.students
+
+docs = students.find()
+print(" -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+
+
+for doc in docs:
+    print("Student ID: " + str(doc["student_id"]))
+    print("First Name: " + str(doc["first_name"]))
+    print("Last Name: " + str(doc["last_name"]))
+    print()
+
+print()
+
+print(" -- DISPLAYING STUDENTS DOCUMENTS FROM find_one() QUERY -- ")
+
+doc = students.find_one({"student_id": "1007"})
+print("Student ID: " + str(doc["student_id"]))
+print("First Name: " + str(doc["first_name"]))
+print("Last Name: " + str(doc["last_name"]))
